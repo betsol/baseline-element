90868cffc197fbcb98ca7103f7116a6d85bf4070
Slava Fomin II <s.fomin@betsol.ru>
2015-07-17 14:56:53 +0300

    Update 2015-07-17T11:56:50.779Z

--- changelog.md
+++
@@ -1,6 +0,0 @@
-# baseline-element changelog
-
-## Version 1.0.0
-(16 July 2015)
-
-- Initial release
--- src/header.js
+++
@@ -1,9 +0,0 @@
-/**
- * <%= pkg.name %> - <%= pkg.description %>
- * @version v<%= pkg.version %>
- * @link <%= pkg.homepage %>
- * @license <%= pkg.license %>
- *
- * @author Slava Fomin II <s.fomin@betsol.ru>
- * @author Vladimir Gaintsev <gaintsev@gmail.com>
- */
